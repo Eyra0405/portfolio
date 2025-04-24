0fde6ee752e42162b73dd2e353b6b6edc15f6eba
Celia <urcolarosel2002@gmail.com>
2025-04-24 10:34:25 +0200

    transiciones

--- script.js
+++ script.js
@@ -19,3 +19,41 @@ document.querySelector('.next-button').addEventListener('click', () => {
 
 // Mostrar el primero al cargar
 showItem(current);
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.1,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries, 
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add("show");
+    appearOnScroll.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Selecciona todas las secciones que quieres que tengan el efecto fade-in
+  const sections = document.querySelectorAll('section:not(#inicio):not(header)');
+
+  // Aplica la clase 'fade-in' a esas secciones
+  sections.forEach(section => {
+    section.classList.add('fade-in');
+  });
+
+  // Añade la clase 'visible' después de un breve retraso para el fade-in
+  setTimeout(() => {
+    sections.forEach(section => {
+      section.classList.add('visible');
+    });
+  }, 100); // El retraso puede ajustarse a tu preferencia
+});
